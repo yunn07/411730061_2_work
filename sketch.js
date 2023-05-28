f4a771166cdbb31dbfce1cfbeba42b9e691ad4ca
yunn07 <115965484+yunn07@users.noreply.github.com>
2023-05-28 21:36:00 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,131 +3,111 @@ let points = [[1,10],[3,11],[4,11],[6,10],[4,9],[3,9],[1,8],[3,9],[4,9],[5,8],[5
 var stroke_colors = "064789-427aa1-ebf2fa-679436-a5be00".split("-").map(a=>"#"+a)
 var fill_colors ="f7d1cd-e8c2ca-d1b3c4-b392ac-735d78".split("-").map(a=>"#"+a)
 
-function preload(){    //最早執行的程式碼
-	bullet_sound = loadSound("sound/boom.mp3")
-  rabbit_sound = loadSound("sound/Boing Sound Effect.mp3")
+//粒子，類別
+class Obj{           //一隻兔子物件的設定
+  constructor(args){     //預設值，基本資料(包含物件顏色，位置，速度，大小...)
+    // this.p =  args.p || {x:random(width),y:random(height)}   //一個物件開始的位置
+    this.p = args.p || createVector(random(width),random(height))
+    // this.v =  {x:random(-1,1),y:random(-1,1)}  //速度，x、y移動的速度為亂數產生-1到1之間的數字
+    this.v =  createVector(random(-1,1),random(-1,1))  //產生一個x座標值為random(-1,1)，y座標值為random(-1,1)
+    this.size = random(10,20)  //放大倍率，物件的大小為亂數產生10到20之間
+    this.color = random(fill_colors)  //亂數產生物件顏色
+    this.stroke = random(stroke_colors) //亂數產生線條顏色
+  }
+  draw()   //把物件畫出來的函數
+  {
+    push()   //重新設定新的原點與顏色設定
+      translate(this.p.x,this.p.y)  //原點設定在物件所在位置
+      scale((this.v.x<0?1:-1),-1)   //放大縮小的指令，左右翻轉==>this.v.x<0?1:-1 ==>this.v.x<0條件成立的話，則值為1，否則為-1
+      fill(this.color)
+      stroke(this.stroke)
+      beginShape()
+        for(var i=0;i<points.length-1;i++){
+          //line(points[i][0]*this.size,points[i][1]*this.size,points[i+1][0]*this.size,points[i+1][1]*this.size)
+          vertex(points[i][0]*this.size,points[i][1]*this.size)
+          //curveVertex(points[i][0]*this.size,points[i][1]*this.size)
+        }
+      endShape()
+    pop()
+  }
+  update(){   //移動後設置位置資料值為何
+    //移動的程式碼+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // this.p.x = this.p.x + this.v.x
+    // this.p.y = this.p.y + this.v.y
+    this.p.add(this.v)   //此行的效果跟上面兩行一樣,add=+
+    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    //算出滑鼠位置的向量
+    let mouseV = createVector(mouseX,mouseY)  //把目前滑鼠的位置轉成向量值
+    //let delta = mouseV.sub(this.p).limit(3)  //delta值紀錄與滑鼠方向移動的"單位"距離,sub微向量減法
+    let delta = mouseV.sub(this.p).limit(this.v.mag()*2)  //與原本物件的速度有關，this.v.mag()==>取得物件的速度值
+    this.p.add(delta)
+
+    //碰壁的處理程式碼++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    if(this.p.x <= 0||this.p.x >= width){     //<0碰到左邊，>width為碰到右邊
+      this.v.x = -this.v.x
+    }
+    if(this.p.y <= 0||this.p.y >= height){     //<0碰到左邊，>width為碰到右邊
+      this.v.y = -this.v.y
+    }
+  }
+  isBallInRanger(){    //判斷有沒有被滑鼠按到
+    let d = dist(mouseX,mouseY,this.p.x,this.p.y)    //計算滑鼠按下的點與此物件位置之間的距離
+    if(d<this.size*4){    //4的由來:去看座標點最大的值，以此作為方框的高與寬
+      return true         //代表距離有在範圍內
+    }else{
+      return false        //代表距離沒有在範圍內
+    }
+  }
+
 }
 
 var ball   //代表單一個物件，利用這個變數來做正在處理的物件
 var balls = []   //陣列，放所有的物件資料，物件倉庫，裡面儲存所有的物件資料
-var bullet
-var bullets = []
-var monster
-var monsters = []
 var score = 0
-var shipP          //設定砲台位置
-
 
 function setup() {   //設定兔子物件倉庫內的資料
-  createCanvas(windowWidth,windowHeight);    //預設砲台的位置為視窗的中間
-  shipP = createVector(width/2,height/2)
+  createCanvas(windowWidth,windowHeight);
   //產生幾個物件
-  for(var j=0;j<10;j=j+1)
+  for(var j=0;j<20;j=j+1)
   {
     ball = new Obj({})   //產生一個新的物件，"暫時"放入到ball變數中
     balls.push(ball)   //把ball物件放入到balls物件倉庫(陣列)中
-  }
-  for(var j=0;j<15;j=j+1)
-  {
-    monster = new Monster({})   //產生一個新的物件，"暫時"放入到monster變數中
-    monsters.push(monster)   //把monster物件放入到monsters物件倉庫(陣列)中
   }
 }
 
 function draw() {   //每秒會執行60次
   background(220);
-  if(keyIsPressed){    //鍵盤是否被按下，如果有鍵盤被按下keyPressed的值為true
-    if(key =="ArrowLeft"){   //按下鍵盤的往左鍵
-      shipP.x = shipP.x-5
-    }
-    if(key =="ArrowRight"){   //按下鍵盤的往右鍵
-      shipP.x = shipP.x+5
-    }
-    if(key =="ArrowUp"){      //按下鍵盤的往上鍵
-      shipP.y = shipP.y-5
-    }
-    if(key =="ArrowDown"){   //按下鍵盤的往下鍵
-      shipP.y = shipP.y+5
-    }
-  }
+  // for(var k=0;k<balls.length;k=k+1){
+  //   ball=balls[k]
+  //   ball.draw()
+  //   ball.update()
+  // }
+
   for(let ball of balls){    //針對陣列變數，取出陣列內一個一個的物件
     ball.draw()
     ball.update()
-    //由此判斷，每隻兔子有沒有接觸每一個飛彈
-    for(let bullet of bullets){
-      if(ball.isBallInRanger(bullet.p.x,bullet.p.y)){  //判斷ball與bullet有沒有碰觸
-        score = score + 1
-        rabbit_sound.play()
-        balls.splice(balls.indexOf(ball),1)           
-        bullets.splice(bullets.indexOf(bullet),1)
-      }
-    }
   }
-
-  for(let bullet of bullets){    //針對飛彈倉庫內的資料，一筆一筆的顯示出來
-    if(monster.IsDead==true){
-      monsters.splice(monsters.indexOf(monster),1)
-    }
-    bullet.draw()
-    bullet.update()
-  }
-  for(let monster of monsters){    //針對飛彈倉庫內的資料，一筆一筆的顯示出來
-    monster.draw()
-    monster.update()
-    for(let bullet of bullets){
-      if(monster.isBallInRanger(bullet.p.x,bullet.p.y)){  //判斷ball與bullet有沒有碰觸
-        score = score - 1
-        //monsters.splice(monsters.indexOf(monster),1)           
-        monster.IsDead = true  //已經被打到了，準備執行爆炸後的畫面
-        bullets.splice(bullets.indexOf(bullet),1)
-      }
-    }
-  }
-  
-
   textSize(50)
   text(score,50,50)
-  //++++++++畫出中間砲台++++++++++++++++++
-  push()
-    let dx = mouseX-width/2    //滑鼠座標到中心點座標的x軸距離
-    let dy = mouseY-height/2   //滑鼠座標到中心點座標的y軸距離
-    let angle = atan2(dy,dx)   //利用反tan算出角度
-
-
-    //translate(width/2,height/2)     //砲台的位置
-    translate(shipP.x,shipP.y)       //砲台的位置,使用shipP的向量值
-    rotate(angle)             //讓三角形翻轉一個angle的角度
-    fill("#ffc03a")
-    noStroke()
-    ellipse(0,0,60)
-    fill("#ff0000")
-    triangle(50,0,-25,-25,-25,25)  //畫出三角形
-    
-  pop()
-  //+++++++++++++++++++++++++++++++++++++
 }
+
+
 function mousePressed(){
-  //按下滑鼠後刪除兔子物件
-  // for(let ball of balls){
-  //   if(ball.isBallInRanger(mouseX,mouseY)){
-  //     把倉庫的這個物件刪除
-  //     score = score+1
-  //     balls.splice(balls.indexOf(ball),1)    //把倉庫內編號第幾個刪除，只刪除一個(indexOf()找出ball的編號)
-  //   }
-  // }
+  //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  //按下滑鼠產生一個物件程式碼
+  // ball = new Obj({
+  //   p:{x:mouseX,y:mouseY}
+  // })   //產生一個新的物件，"暫時"放入到ball變數中
+  // balls.push(ball)   //把ball物件放入到balls物件倉庫(陣列)中
+  //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-  //新增一筆飛彈資料(還沒有顯示)
-  bullet = new Bullet({})
-  bullets.push(bullet)   //把這一筆資料放入飛彈倉庫
-  bullet_sound.play()
-}
-
-function keyPressed(){
-  if(key ==" "){
-    bullet = new Bullet({})
-    bullets.push(bullet)   //把這一筆資料放入飛彈倉庫
-    bullet_sound.play()
+  //按下滑鼠後刪除該物件
+  for(let ball of balls){
+    if(ball.isBallInRanger()){
+      //把倉庫的這個物件刪除
+      score = score+1
+      balls.splice(balls.indexOf(ball),1)    //把倉庫內編號第幾個刪除，只刪除一個(indexOf()找出ball的編號)
     }
-  
+  }
 }
-
